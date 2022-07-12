f1a190b18032045653c41bbf64128dd36d217c7f
sean <yechunxiao269@gmail.com>
2022-07-12 14:15:19 +0800

    Update

---
+++ js/mathjax-config.js
@@ -0,0 +1,21 @@
+// MathJax Configuration
+//
+// v2 to v3 upgrade notes:
+// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
+// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
+window.MathJax = {
+  tex: {
+    macros: {
+      R: "\\mathbb{R}", E: "\\mathrm{E}", x: "\\mathrm{x}", y: "\\mathrm{y}",
+      d: "\\mathrm{d}", Var: "\\mathrm{Var}", Cov: "\\mathrm{Cov}",
+      rank: "\\mathrm{rank}", tr: "\\mathrm{tr}",
+      arccot: "\\mathrm{arccot}"
+    },
+    displayMath: [['$$', '$$'], ['\\[', '\\]']],
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    maxBuffer: 10 * 1024,
+    processEscapes: false,
+    processEnvironments: false,
+    tags: "all"
+  }
+}
